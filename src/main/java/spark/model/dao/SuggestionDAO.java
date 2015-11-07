abd022b3c9397b2c463e05abfa5ec1b986682071
ravendestiny <ravendestiny@users.noreply.github.com>
2015-11-07 16:11:34 +0100

    Implement Engine Suggestion

---
+++ src/main/java/spark/model/dao/SuggestionDAO.java
@@ -0,0 +1,30 @@
+package spark.model.dao;
+
+import java.util.List;
+
+import org.hibernate.search.query.dsl.QueryBuilder;
+
+import spark.Constant;
+import spark.model.bean.Suggestion;
+
+public class SuggestionDAO extends Dao<Suggestion> {
+	
+	@SuppressWarnings("unchecked")
+	public List<Suggestion> getByQueryOrderByScoring(String query) {
+		QueryBuilder queryBuilder = fullTextEntityManager.getSearchFactory().buildQueryBuilder()
+				.forEntity(Suggestion.class).get();
+		
+		org.apache.lucene.search.Query luceneQuery = queryBuilder
+				.phrase()
+				.withSlop(2)
+				.onField("queryNgram")
+				.andField("queryEdgeNgram").boostedTo(5)
+				.sentence(query)
+				.createQuery();
+
+		return (List<Suggestion>) fullTextEntityManager.createFullTextQuery(luceneQuery, Suggestion.class)
+				.setMaxResults(Constant.SUGGESTION_MAXRESULT)
+				.getResultList();
+	}
+
+}
