7c4451364cd7cc161b825076c698e885f82c2380
ravendestiny <ravendestiny@users.noreply.github.com>
2015-12-09 15:52:49 +0100

    Database Mapping => Edit Document => Edit User => Create Author => Create Source => Create Session => Create Query

--- src/main/java/spark/model/bean/Document.java
+++ src/main/java/spark/model/bean/Document.java
@@ -1,11 +1,20 @@
 package spark.model.bean;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import org.apache.lucene.analysis.core.KeywordTokenizerFactory;
 import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
@@ -18,13 +27,16 @@ import org.hibernate.search.annotations.Analyze;
 import org.hibernate.search.annotations.Analyzer;
 import org.hibernate.search.annotations.AnalyzerDef;
 import org.hibernate.search.annotations.AnalyzerDefs;
+import org.hibernate.search.annotations.DateBridge;
 import org.hibernate.search.annotations.DocumentId;
 import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.FieldBridge;
 import org.hibernate.search.annotations.Fields;
 import org.hibernate.search.annotations.Index;
 import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.IndexedEmbedded;
 import org.hibernate.search.annotations.Parameter;
+import org.hibernate.search.annotations.Resolution;
 import org.hibernate.search.annotations.Store;
 import org.hibernate.search.annotations.TermVector;
 import org.hibernate.search.annotations.TokenFilterDef;
@@ -71,6 +83,7 @@ public class Document {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "DOCUMENT_ID")
 	@DocumentId
 	private long id;
 	
@@ -118,7 +131,28 @@ public class Document {
 	@FieldBridge(impl = PdfBridge.class)
 	private String attachment;
 	
+	@Column(name = "DOCUMENT_PUBLICATIONDATE", nullable = false)
+	@Field(index = Index.YES, store = Store.YES, analyze = Analyze.NO)
+	private Date publicationDate;
 	
+	@Column(name = "DOCUMENT_UPDATEDATE", nullable = false)
+	@DateBridge(resolution = Resolution.YEAR)
+	private Date updateDate;
+	
+	@ManyToMany
+	@JoinTable(name = "SPARK_DOCUMENTAUTHOR",
+		joinColumns = @JoinColumn(name = "DOCUMENTAUTHOR_DOCUMENTID", referencedColumnName = "DOCUMENT_ID"),
+		inverseJoinColumns = @JoinColumn(name = "DOCUMENTAUTHOR_AUTHORID", referencedColumnName = "AUTHOR_ID"),
+		uniqueConstraints = @UniqueConstraint(columnNames = {"DOCUMENTAUTHOR_DOCUMENTID", "DOCUMENTAUTHOR_AUTHORID"})
+	)
+	@IndexedEmbedded
+	private Set<Author> authors = new HashSet<Author>();
+	
+	@ManyToOne(targetEntity = Source.class)
+	@JoinColumn(name = "DOCUMENT_SOURCEID")
+	private Source source;
+	
+
 	public long getId() {
 		return id;
 	}
@@ -145,6 +179,38 @@ public class Document {
 	
 	public void setAttachment(String attachment) {
 		this.attachment = attachment;
+	}
+	
+	public Date getPublicationDate() {
+		return publicationDate;
+	}
+
+	public void setPublicationDate(Date publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}
+	
+	public Source getSource() {
+		return source;
+	}
+
+	public void setSource(Source source) {
+		this.source = source;
 	}
 	
 }
