7872ae3e6c94757f0333ade85f030ea160353a98
edouard-jacquet <9915104+edouard-jacquet@users.noreply.github.com>
2015-12-12 18:37:30 +0100

    Add User instance

--- src/main/java/spark/deployer/DatabaseDeployer.java
+++ src/main/java/spark/deployer/DatabaseDeployer.java
@@ -3,7 +3,9 @@ package spark.deployer;
 import org.jboss.logging.Logger;
 
 import spark.model.bean.Source;
+import spark.model.bean.User;
 import spark.model.dao.SourceDAO;
+import spark.model.dao.UserDAO;
 
 public class DatabaseDeployer extends Deployer {
 	
@@ -22,6 +24,23 @@ public class DatabaseDeployer extends Deployer {
 		System.out.println("----------");
 		System.out.println("--------------- DATABASE DEPLOY START ----------");
 		System.out.println("----------");
+		
+		UserDAO userDAO = new UserDAO();
+		if(userDAO.getAll().size() == 0) {
+			String[][] users = {
+				{"admin", "admin", "####"}
+			};
+			
+			for(String[] entry : users) {
+				User user = new User();
+				user.setLogin(entry[0]);
+				user.setPassword(entry[1]);
+				user.setKey(entry[2]);
+				userDAO.create(user);
+			}
+			
+			logger.info(users.length +" users have been added.");
+		}
 
 		SourceDAO sourceDAO = new SourceDAO();
 		if(sourceDAO.getAll().size() == 0) {
