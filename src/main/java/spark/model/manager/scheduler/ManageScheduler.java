a768c158142c554eefd26d5e57f8917039bc8687
Grizoune <grisonsylvain@gmail.com>
2016-02-09 09:53:00 +0100

    Ajout de la sauvegarde des parametres Ajout de la gestion des auteurs correctifs

--- src/main/java/spark/model/manager/scheduler/ManageScheduler.java
+++ src/main/java/spark/model/manager/scheduler/ManageScheduler.java
@@ -4,32 +4,34 @@ import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SchedulerFactory;
-import org.quartz.SimpleTrigger;
-import org.quartz.Trigger;
-import org.quartz.impl.JobDetailImpl;
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
-import static org.quartz.SimpleScheduleBuilder.*;
 
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.quartz.CronTrigger;
+import static org.quartz.CronScheduleBuilder.*;
+
+import spark.model.dao.ConfigurationDAO;
 import spark.model.manager.scheduler.job.UpdateLibraryJob;
 
 public class ManageScheduler {
 	
-	public static void initScheduler() throws SchedulerException{
+	public static void initScheduler() throws SchedulerException, JSONException{
 		SchedulerFactory schedFact = new org.quartz.impl.StdSchedulerFactory();
 		Scheduler sched = schedFact.getScheduler();
 		sched.start();
-		
 		JobDetail job1 = newJob(UpdateLibraryJob.class).withIdentity("updateJob", "group1").build(); 
-		Trigger trigger = newTrigger()
-				.withIdentity("myTrigger", "group1")
-				.startNow()
-				.withSchedule(simpleSchedule()
-				.withIntervalInSeconds(40)
-				.repeatForever())
-				.build(); 
 		
-		sched.scheduleJob(job1, trigger);
+		ConfigurationDAO config_dao = new ConfigurationDAO();
+		JSONObject config = new JSONObject(config_dao.getByKey("schedule").getValue());
+		String configTrigger = config.getString("trigger");
+		
+		CronTrigger trigger = newTrigger()
+				.withIdentity("myTrigger", "group1")
+				.withSchedule(cronSchedule(configTrigger))
+				.forJob(job1)
+				.build();
 		
 		System.out.println("--------------- INITIATION DU SCHEDULER ----------");
 	}
